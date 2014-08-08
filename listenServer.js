846251dc5c87d71d0280ca39b96862ce604e0afd
Aayush Chhabra <chhabraaayush@yahoo.ca>
2014-08-08 11:42:45 -0400

    notifications working

--- listenServer.js
+++ listenServer.js
@@ -3,12 +3,15 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var messageController = require("./messageController");
+var Notification = require('node-notifier');
+var notifier = new Notification();
 
 app.use(express.static(__dirname + '/public'));
 
 // app.get('/', function(req, res){
 //   res.redirect('index.html');
 // });
+
 
 app.get("/:number/:message", function(req, res){
 
@@ -23,7 +26,17 @@ app.post("/hello", function(req, res) {
         body += chunk;
     });
     req.on('end', function() {
-        console.log(body);
+        //console.log(body);
+        body = JSON.parse(body);
+        body = parseInt(body.Number) + " : " +body.Message;
+        console.log(__dirname+"/public");
+        notifier.notify({
+            "title": "WatsApp!!",
+            "message": body,
+            "appIcon": __dirname + "/public/icon.png"
+        }, function(error, response){
+            console.log(error, response);
+        });
         io.emit("chat message", body);
         res.writeHead(200);
         res.end("Server got the message.");
@@ -39,8 +52,15 @@ io.on('connection', function(socket){
 });
 
 io.on("message", function(number, message){
-    console.log(number, message);
+    //console.log(number, message);
+    a();
 })
+
+
+function a(){
+    console.log("in fuction a");
+}
+
 
 http.listen(8080, function(){
   console.log('listening on *:8080');
